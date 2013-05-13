882559d8205fd046087a360955445072bcc954a9
Maik Kempe <dev@nrolla.com>
2013-05-13 21:48:15 +0200

    [✔] Uppppsssss, something was missing.

--- lib/middleman-wundertuete/extension.rb
+++ lib/middleman-wundertuete/extension.rb
@@ -1,7 +1,6 @@
 # encoding: utf-8
 
 require 'middleman-wundertuete/extension/options'
-
 require 'middleman-wundertuete/helpers/layout_helper'
 require 'middleman-wundertuete/helpers/text_helper'
 
@@ -15,15 +14,10 @@ module Middleman
 
         app.send :include, Helpers # Include helpers
 
-        vendor_assets = File.expand_path('../../../vendor/assets', __FILE__)
-
-        sprockets.append_path "#{vendor_assets}/fonts"
-        sprockets.append_path "#{vendor_assets}/images"
-        sprockets.append_path "#{vendor_assets}/javascripts"
-        sprockets.append_path "#{vendor_assets}/stylesheets"
-
         app.after_configuration do # Once configuration is parsed
-          # ...
+          vendor_assets = File.expand_path('../../../vendor/assets', __FILE__)
+
+          %w[fonts images javascripts stylesheets].each { |type| sprockets.append_path "#{vendor_assets}/#{type}" }
         end
       end
 
