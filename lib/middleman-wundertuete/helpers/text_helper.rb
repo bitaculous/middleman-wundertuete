1358332a5009e048856ab69106c33583f2474266
Maik Kempe <mkempe@bitaculous.com>
2015-05-20 20:45:25 +0200

    [➠] Added `handleize` helper.

--- lib/middleman-wundertuete/helpers/text_helper.rb
+++ lib/middleman-wundertuete/helpers/text_helper.rb
@@ -2,6 +2,21 @@ module Middleman
   module Wundertuete
     module Helpers
       module TextHelper
+        # Public: Handleize an input.
+        #
+        # input     - The String input.
+        # delimiter - The String delimiter (default: '-').
+        #
+        # Examples
+        #
+        #   handleize 'Bob Ross'
+        #   # => 'bob-ross'
+        #
+        # Returns the handleized String.
+        def handleize(input, delimiter = '-')
+          input.to_str.gsub(/[ \_\-\/]/, delimiter).downcase
+        end
+
         # Public: Generates a copyright.
         #
         # owner - The String naming the copyright owner.
@@ -13,15 +28,19 @@ module Middleman
         #
         # Examples
         #
-        #   copyright('Bob Ross') → '©2013 Bob Ross - All rights reserved.'
+        #   copyright 'Bob Ross'
+        #   # => '©2013 Bob Ross - All rights reserved.'
         #
-        #   copyright('Bob Ross', { symbol: '(c)' }) → '(c)2013 Bob Ross - All rights reserved.'
+        #   copyright 'Bob Ross', { symbol: '(c)' }
+        #   # => '(c)2013 Bob Ross - All rights reserved.'
         #
-        #   copyright('Bob Ross', { claim: 'All your rights are belong to us.' }) → '©2013 Bob Ross - All your rights are belong to us.'
+        #   copyright 'Bob Ross', { claim: 'All your rights are belong to us.' }
+        #   # => '©2013 Bob Ross - All your rights are belong to us.'
         #
-        #   copyright('Bob Ross', { initial_year: '2006' }) → '©2006-2013 Bob Ross - All rights reserved.'
+        #   copyright 'Bob Ross', { initial_year: '2006' }
+        #   # => '©2006-2013 Bob Ross - All rights reserved.'
         #
-        # Returns the copyright as String.
+        # Returns the copyright String.
         def copyright(owner, options = {})
           if owner
             time         = Time.new
